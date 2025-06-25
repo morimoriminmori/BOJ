717668782d0e6cc18a96cd8c215028923be5fe81
morimoriminmori <ngmmaenter@gmail.com>
2025-06-25 15:42:51 +0900

    Create 1000.cpp

---
+++ Bronze/1000.cpp
@@ -0,0 +1,38 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define pii pair<int,int>
+#define fastio ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0)
+#define endl "\n"
+void __main();
+const long long INF = 1e18;
+int main(){
+  fastio;
+  __main();
+  return 0;
+}
+#define ll long long
+#define ull unsigned long long
+#define pb push_back
+#define all(x) x.begin(), x.end()
+#define F first
+#define S second
+#define mp make_pair
+#define setbits(x) __builtin_popcountll(x)
+#define sz(x) (int)x.size()
+#define rep(i, a, b) for (int i = a; i < b; i++)
+#define rep1(i, a, b) for (int i = a; i <= b; i++)
+#define per(i, a, b) for (int i = a; i > b; i--)
+#define per1(i, a, b) for (int i = a; i >= b; i--)
+#define debug(x) cout << #x << " = " << x << endl
+#define debug2(x, y) cout << #x << " = " << x << ", " << #y << " = " << y << endl
+#define debug3(x, y, z) cout << #x << " = " << x << ", " << #y << " = " << y << ", " << #z << " = " << z << endl
+#define lowerbound(x, y) lower_bound(all(x), y) - x.begin()
+#define upperbound(x, y) upper_bound(all(x), y) - x.begin()
+#define setbits(x) __builtin_popcountll(x)
+#define poly vector<ll>
+#define poly2 vector<pii>
+void __main() {
+  int n,m;
+  cin >> n >> m;
+  cout << n + m;
+}
