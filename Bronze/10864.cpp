5a01e1f7ed89c05b1b608db240c305342bd7c3bc
morimoriminmori <ngmmaenter@gmail.com>
2025-06-25 15:47:04 +0900

    Create 10864.cpp

---
+++ Bronze/10864.cpp
@@ -0,0 +1,44 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define pii pair<int,int>
+#define fastio ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0)
+#define endl "\n"
+void __main();
+const long long INF = 1e18;
+int main(){
+  fastio;
+  __main();
+  return 0;
+}
+#define ll long long
+#define ull unsigned long long
+#define pb push_back
+#define all(x) x.begin(), x.end()
+#define uniq(x) sort(all(x));x.erase(unique(all(x)), x.end())
+#define F first
+#define S second
+#define mp make_pair
+#define MAX 1000002
+#define MOD 1000000007
+#define PI 3.141592653589793238462643383279502884L
+#define EPS 1e-9
+#define loop while(1)
+#define vi vector<ll>
+#define i128 __int128
+#define vpi vector<pii>
+#define vvi vector<vector<ll>>
+#define ld long double
+
+int n,m,a,b;
+vi cnt(101010);
+void __main() {
+  cin >> n >> m;
+  for (int i=0;i<m;i++){
+    cin >> a >> b;
+    cnt[a]+=1;
+    cnt[b]+=1;
+  }
+  for (int i=1;i<=n;i++) {
+    cout << cnt[i] << endl;
+  }
+}
